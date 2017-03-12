d87f97426a217d4abc3c8fbce0ccb04795df7492
jonathanjfshaw <jonathanjfshaw@gmail.com>
2017-03-12 19:54:46 +0000

    Assume times are intended to be Drupal time

--- src/Drupal/Driver/Fields/Drupal8/DatetimeHandler.php
+++ src/Drupal/Driver/Fields/Drupal8/DatetimeHandler.php
@@ -11,10 +11,22 @@ class DatetimeHandler extends AbstractHandler {
    * {@inheritdoc}
    */
   public function expand($values) {
+    // A Drupal install has a default user-facing timezone, but nonetheless
+    // uses UTC for internal storage. If no timezone is specified in a date
+    // field value by the step author, assume it is in the default timezone of
+    // the Drupal install, and therefore transform it into UTC for storage.
+    $dtz = \Drupal::config('system.date')->get('timezone.default');
+    // Drupal timezone is not always set, see
+    // https://api.drupal.org/api/drupal/core!includes!bootstrap.inc/function/drupal_get_user_timezone/8.2.x
+    // Ignore PHP strict notice if timezone has not yet been set in the php.ini.
+    $dtz = !empty($dtz) ? $dtz : @date_default_timezone_get();
+    $dtz = new DateTimeZone($dtz);
+    $utc = new DateTimeZone('UTC');
     foreach ($values as $key => $value) {
-      $values[$key] = str_replace(' ', 'T', $value);
+      $date = new DateTime($value, $dtz);
+      $date->setTimezone($utc);
+      $values[$key] = $date->format('Y-m-d\TH:i:s');
     }
-    return $values;
-  }
+    return $values;  }
 
 }
